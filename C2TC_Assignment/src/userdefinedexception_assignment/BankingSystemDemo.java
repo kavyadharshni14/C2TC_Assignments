6d2588ad62690ae9d704fe0595460f704ee0906f
kavya <kavya@LAPTOP-EEGOMD4V>
2025-08-28 20:49:19 +0530

    userdefined exception

---
+++ C2TC_Assignment/src/userdefinedexception_assignment/BankingSystemDemo.java
@@ -0,0 +1,10 @@
+package userdefinedexception_assignment;
+
+public class BankingSystemDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C2TC_Assignment/src/userdefinedexception_assignment/InsufficientFundsException.java
@@ -0,0 +1,10 @@
+package userdefinedexception_assignment;
+
+public class InsufficientFundsException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
